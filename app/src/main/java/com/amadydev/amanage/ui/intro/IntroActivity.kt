75829bd4bcb8f71b2c012b937c574756c5ed8560
Jeff Noelson Amady <jeff02.jn@gmail.com>
2022-01-16 17:52:36 -0300

    sign up

--- app/src/main/java/com/amadydev/amanage/ui/intro/IntroActivity.kt
+++ app/src/main/java/com/amadydev/amanage/ui/intro/IntroActivity.kt
@@ -1,12 +1,27 @@
 package com.amadydev.amanage.ui.intro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.amadydev.amanage.R
+import com.amadydev.amanage.databinding.ActivityIntroBinding
+import com.amadydev.amanage.ui.signup.SignUpActivity
 
 class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        with(binding) {
+            btnSignUp.setOnClickListener {
+                startActivity(Intent(this@IntroActivity, SignUpActivity::class.java))
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/amadydev/amanage/ui/signup/SignUpActivity.kt
@@ -0,0 +1,30 @@
+package com.amadydev.amanage.ui.signup
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.amadydev.amanage.R
+import com.amadydev.amanage.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(binding.toolbarSignUpActivity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+        }
+
+        binding.toolbarSignUpActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+}
