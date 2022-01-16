ed07910c06ecbc970065dec22105a584bb9d7eb5
Jeff <75714102+jeffamady@users.noreply.github.com>
2022-01-16 00:02:30 -0300

    Develop (#1)
    * vcs
    
    * misc
    
    * gradle
    
    * compiler
    
    * splash

---
+++ app/src/main/java/com/amadydev/amanage/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.amadydev.amanage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+    }
+}
