f1d44e696dd753d36dd752a5d45872c1c262fe30
Jeff Noelson Amady <jeff02.jn@gmail.com>
2022-01-16 03:03:27 -0300

    intro

--- app/src/main/java/com/amadydev/amanage/SplashActivity.kt
+++ app/src/main/java/com/amadydev/amanage/SplashActivity.kt
@@ -1,13 +1,21 @@
 package com.amadydev.amanage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.WindowManager
+import androidx.core.os.HandlerCompat.postDelayed
+import com.amadydev.amanage.ui.intro.IntroActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+        },2500)
     }
 }
---
+++ app/src/main/java/com/amadydev/amanage/ui/intro/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.amadydev.amanage.ui.intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.amadydev.amanage.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
