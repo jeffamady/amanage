0ff9f06deab1d0fd555beef9c0443d4f9e3e8878
Jeff Amady <jeff02.jn@gmail.com>
2022-01-19 20:24:26 -0300

    login user

--- app/src/main/java/com/amadydev/amanage/firebase/FirestoreDB.kt
+++ app/src/main/java/com/amadydev/amanage/firebase/FirestoreDB.kt
@@ -1,6 +1,7 @@
 package com.amadydev.amanage.firebase
 
 import com.amadydev.amanage.firebase.model.User
+import com.amadydev.amanage.ui.signin.SignInViewModel
 import com.amadydev.amanage.ui.signup.SignUpViewModel
 import com.amadydev.amanage.utils.Constants.USERS
 import com.google.firebase.auth.ktx.auth
@@ -15,7 +16,24 @@ class FirestoreDB {
         db.collection(USERS)
             .document(getCurrentUserId()).set(user, SetOptions.merge())
             .addOnSuccessListener {
-                viewModel.userRegisteredSuccess()
+                viewModel.userRegisteredSuccess(true)
+            }
+            .addOnFailureListener {
+                it.message?.let { message -> viewModel.userRegisteredSuccess(false, message) }
+            }
+    }
+
+    fun loginUser(viewModel: SignInViewModel) {
+        db.collection(USERS)
+            .document(getCurrentUserId())
+            .get()
+            .addOnSuccessListener { document ->
+                document.toObject(User::class.java)?.let { loggedUser ->
+                    viewModel.loginSuccess(true, loggedUser)
+                }
+            }
+            .addOnFailureListener {
+                viewModel.loginSuccess(false)
             }
     }
 
