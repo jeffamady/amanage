8e72fb3d37999031f132aeca54a20a9e613e4833
Jeff Amady <jeff02.jn@gmail.com>
2022-01-19 01:55:29 -0300

    observe sign in view state

--- app/src/main/java/com/amadydev/amanage/ui/signin/SignInActivity.kt
+++ app/src/main/java/com/amadydev/amanage/ui/signin/SignInActivity.kt
@@ -1,20 +1,84 @@
 package com.amadydev.amanage.ui.signin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import com.amadydev.amanage.R
 import com.amadydev.amanage.databinding.ActivitySignInBinding
+import com.amadydev.amanage.ui.BaseActivity
+import com.amadydev.amanage.utils.afterTextChanged
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : BaseActivity() {
     private lateinit var binding: ActivitySignInBinding
+    private val signInViewModel: SignInViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setupActionBar()
+        validateData()
+        setListeners(false)
+        setObservers()
+    }
+
+    private fun setObservers() {
+        signInViewModel.signInState.observe(this) {
+            with(binding){
+                when(it){
+                    is SignInViewModel.SignInState.EmailError ->
+                        etEmail.error = getString(it.resourceId)
+                        SignInViewModel.SignInState.Error ->
+                            showErrorSnackBar(root, getString(R.string.sorry))
+                    is SignInViewModel.SignInState.IsFormValid ->
+                        setListeners(it.isFormValid)
+                    is SignInViewModel.SignInState.Loading ->
+                        showProgressDialog(it.isLoading)
+                    is SignInViewModel.SignInState.NonSuccess ->
+                        showErrorSnackBar(root, it.message)
+                    is SignInViewModel.SignInState.PasswordError ->
+                        etPassword.error = getString(it.resourceId)
+                    is SignInViewModel.SignInState.Success ->
+                        Toast.makeText(this@SignInActivity, it.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+    }
+
+    private fun setListeners(isFormValid: Boolean) {
+        binding.btnSignIn.setOnClickListener {
+            when {
+                isFormValid -> {
+                    signInViewModel.loginUser(
+                        binding.etEmail.text.toString(),
+                        binding.etPassword.text.toString()
+                    )
+                }
+                else -> showErrorSnackBar(it, "You have to complete all")
+            }
+
+        }
+    }
+
+    private fun validateData() {
+        with(binding) {
+            etEmail.afterTextChanged {
+                signInViewModel.validateForm(
+                    etEmail.text.toString(),
+                    etPassword.text.toString()
+                )
+            }
+            etPassword.afterTextChanged {
+                signInViewModel.validateForm(
+                    etEmail.text.toString(),
+                    etPassword.text.toString()
+                )
+            }
+        }
     }
 
     private fun setupActionBar() {
