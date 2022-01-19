d12adda887a067c6462f7ffa0c3f6e536b02df86
Jeff Amady <jeff02.jn@gmail.com>
2022-01-19 18:35:54 -0300

    User model

---
+++ app/src/main/java/com/amadydev/amanage/firebase/model/User.kt
@@ -0,0 +1,15 @@
+package com.amadydev.amanage.firebase.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class User(
+    val id: String = "",
+    val name: String = "",
+    val email: String = "",
+    val image: String = "",
+    val mobile: Long = 0,
+    val fcmToken: String = ""
+
+): Parcelable
