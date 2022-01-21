48afd7606c6afdcf1575846d4946637724c56842
Jeff Amady <jeff02.jn@gmail.com>
2022-01-21 16:18:50 -0300

    image chooser

--- app/src/main/java/com/amadydev/amanage/ui/myprofile/MyProfileActivity.kt
+++ app/src/main/java/com/amadydev/amanage/ui/myprofile/MyProfileActivity.kt
@@ -1,9 +1,16 @@
 package com.amadydev.amanage.ui.myprofile
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.get
+import androidx.core.view.isVisible
 import com.amadydev.amanage.R
 import com.amadydev.amanage.data.model.User
 import com.amadydev.amanage.databinding.ActivityMyProfileBinding
@@ -12,7 +19,12 @@ import com.bumptech.glide.Glide
 
 class MyProfileActivity : BaseActivity() {
     private lateinit var binding: ActivityMyProfileBinding
-    private val myProfileViewModel : MyProfileViewModel by viewModels()
+    private val myProfileViewModel: MyProfileViewModel by viewModels()
+
+    private var selectPictureLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            uri?.let { myProfileViewModel.setImageUri(it) }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +34,7 @@ class MyProfileActivity : BaseActivity() {
         setupActionBar()
         getUser()
         setupObservers()
+        setListeners()
     }
 
     private fun setupActionBar() {
@@ -44,8 +57,21 @@ class MyProfileActivity : BaseActivity() {
             when (it) {
                 is MyProfileViewModel.MyProfileState.ProfileUser ->
                     setUserData(it.user)
+                is MyProfileViewModel.MyProfileState.Loading -> {
+                    showProgressAndData(it.isLoading)
+                }
+                is MyProfileViewModel.MyProfileState.ImageUri ->
+                    updateImage(it.uri)
             }
         }
+    }
+
+    private fun updateImage(uri: Uri) {
+        Glide.with(this)
+            .load(uri)
+            .error(R.drawable.ic_user_place_holder)
+            .centerCrop()
+            .into(binding.ivUser)
     }
 
     private fun setUserData(user: User) {
@@ -62,5 +88,55 @@ class MyProfileActivity : BaseActivity() {
                 etPhone.setText(user.mobile.toString())
             }
         }
+    }
+
+    private fun setListeners() {
+        with(binding) {
+            ivUser.setOnClickListener {
+                if (ContextCompat.checkSelfPermission(
+                        this@MyProfileActivity, Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+                    == PackageManager.PERMISSION_GRANTED
+                ) {
+                    //Show image chooser
+                    showImageChooser()
+                } else {
+                    ActivityCompat.requestPermissions(
+                        this@MyProfileActivity,
+                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                        READ_STORAGE_IMAGE_PERMISSION_CODE
+                    )
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == READ_STORAGE_IMAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                //Show image chooser
+                showImageChooser()
+            } else {
+                showErrorSnackBar(binding.root, getString(R.string.error_permission_storage))
+            }
+        }
+    }
+
+    private fun showImageChooser() =
+        selectPictureLauncher.launch("image/*")
+
+    private fun showProgressAndData(isLoading: Boolean) {
+        binding.iProgress.root.isVisible = isLoading
+    }
+
+    companion object {
+        private const val READ_STORAGE_IMAGE_PERMISSION_CODE = 1
     }
 }
--- app/src/main/java/com/amadydev/amanage/ui/myprofile/MyProfileViewModel.kt
+++ app/src/main/java/com/amadydev/amanage/ui/myprofile/MyProfileViewModel.kt
@@ -1,5 +1,6 @@
 package com.amadydev.amanage.ui.myprofile
 
+import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -14,14 +15,22 @@ class MyProfileViewModel @Inject constructor()  : ViewModel() {
     val myProfileState : LiveData<MyProfileState> = _myProfileState
 
     fun updateProfileUser(loggedUser: User) {
+        _myProfileState.value = MyProfileState.Loading(false)
         _myProfileState.value = MyProfileState.ProfileUser(loggedUser)
     }
 
     fun getUser() {
+        _myProfileState.value = MyProfileState.Loading(true)
         FirestoreDB().loadUserData(this)
+    }
+
+    fun setImageUri(uri: Uri) {
+        _myProfileState.value = MyProfileState.ImageUri(uri)
     }
 
     sealed class MyProfileState {
         data class ProfileUser(val user: User): MyProfileState()
+        data class Loading(val isLoading: Boolean): MyProfileState()
+        data class ImageUri(val uri: Uri): MyProfileState()
     }
 }
